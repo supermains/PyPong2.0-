0f6bae2520a99c520bd770c44b7c594897280864
2016haoxuesheng <65386804+2016haoxuesheng@users.noreply.github.com>
2020-06-26 16:21:02 +0800

    Update ball.py

--- Game1/ball.py
+++ Game1/ball.py
@@ -1,7 +1,7 @@
 # PyPong2.0.1:1V1乒乓比赛（急速模式）
 # 作者：17好学生(QQ名), 邮箱：wen_zihao@qq.com
 # 支持BSD开源协议,你们可以下载后更改代码
-# 下载地址:
+# 下载地址:https://github.com/2016haoxuesheng/PyPong2.0-/blob/PyPong2.0_VS/Game1/ball.py
 # PS:本版本会分成几个文件来写,更利于学习,记得全部下载!!!
 # 本文件为乒乓球定义文件,下载后无需执行!
 
