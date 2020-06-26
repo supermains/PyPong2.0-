e17b3a4fdfc782d085c62c0a5452fcaaf474fd9b
2016haoxuesheng <65386804+2016haoxuesheng@users.noreply.github.com>
2020-06-26 16:23:35 +0800

    Update settings.py

--- Game1/settings.py
+++ Game1/settings.py
@@ -1,7 +1,7 @@
 # PyPong2.0.1:1V1乒乓比赛（急速模式）
 # 作者：17好学生(QQ名), 邮箱：wen_zihao@qq.com
 # 支持BSD开源协议,你们可以下载后更改代码
-# 下载地址:
+# 下载地址:https://github.com/2016haoxuesheng/PyPong2.0-/blob/PyPong2.0_VS/Game1/settings.py
 # PS:本版本会分成几个文件来写,更利于学习,记得全部下载!!!
 # 本文件为配置文件，下载后无需执行!
 
